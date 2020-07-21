b8c289b49ba802a782c53a9468ccbe33721cac57
John <50677710+jhnwr@users.noreply.github.com>
2020-07-21 18:10:42 +0100

    Update beerwulf.py
    updated to include functions

--- beerwulf.py
+++ beerwulf.py
@@ -1,50 +1,47 @@
-import requests
 from requests_html import HTMLSession
 import pandas as pd
 
 url = 'https://www.beerwulf.com/en-gb/c/subkegs?container=SUB%20Kegs%20-%202L%20keg&catalogCode=Beer_1'
-
-#Use requests_HTML to create a session, then use render() to load the dynamic content
-print('Rendering Page.')
-s = HTMLSession()
-r = s.get(url)
-r.html.render(sleep=1) # sleep=1 is important - sleeps for x seconds after the render
-
-#I used the xpath function to find the main product container. could have used a css selector
-products = r.html.xpath('//*[@id="product-items-container"]/a')
-
 beerlist = []
 
-# Print statement so we can see it working
-print('Getting beerwulf info..')
+def render_page(url):
+    print('Rendering Page.')
+    s = HTMLSession()
+    r = s.get(url)
+    r.html.render(sleep=1) # sleep=1 is important - sleeps for x seconds after the render
+    products = r.html.xpath('//*[@id="product-items-container"]/a')
+    return products
 
-#loop through each element within the products xpath
-for item in products:
-    name  = item.find('h4', first=True).text # return the first item from the list, without this we would get all the H4 tags as a list
-    price = item.find('span', first=True).text
-    beer_type = item.find('p', first=True).text
-    links = item.absolute_links # Pulls in the full urls
-    
-    # because the button has unique text we can search the item html for that string and if it returns, it is out of stock, if it isnt found, the item is in stock
-    # IF statement for the above
-    if item.search('Temporarily sold out'):
-        stock = 'out of stock'
-    else:
-        stock = 'in stock'
-        
-    
-    #print(dict(name=name,stock=stock)) # test string to get stock working
+def parse(products):
+    print('Getting beerwulf info..')
+    for item in products:
+        name  = item.find('h4', first=True).text
+        beer_type = item.find('p', first=True).text
+        price = item.find('span', first=True).text
+        links = item.absolute_links
 
-    product_info = {
-    'Name': name,
-    'Price': price,
-    'Beer Type': beer_type,
-    'Stock': stock,
-    'URL': links
-    }
-    beerlist.append(product_info)
+        if item.search('Temporarily sold out'):
+            stock = 'out of stock'
+        else:
+            stock = 'in stock'
 
-    print(f'Found {len(beerlist)} item(s)')
+        product_info = {
+        'Name': name,
+        'Price': price,
+        'Beer Type': beer_type,
+        'Stock': stock,
+        'URL': links
+        }
+        beerlist.append(product_info)
 
-df = pd.DataFrame(beerlist)
-df.to_excel('beer-list.xls', index=False)
+        print(f'Found {len(beerlist)} item(s)')
+
+def output():
+    df = pd.DataFrame(beerlist)
+    print('Saved to .xls')
+    df.to_excel('beer-list.xls', index=False)
+
+
+html = render_page(url)
+parse(html)
+output()
